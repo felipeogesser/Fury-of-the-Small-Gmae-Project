2cf1e693b381d6bc54f490c46ee388302b7dbc24
felipeogesser <felipeogesser@gmail.com>
2026-04-09 19:44:11 -0300

    corrected syntax error

--- playerMapEdgeCollisionFunc.c
+++ playerMapEdgeCollisionFunc.c
@@ -14,34 +14,34 @@ Map *map = get_map(map_id);
 PlayerMapEdgeCollision(Player *player, Map *map, GameState *game) {
 
     if (player->PlayerWindowPositionX <= map->MapLeftLimit - game->vydt) {
-        gmae->LX = map->MapLeftLimit - player->PlayerWindowPositionX + 1;
+        game->LX = map->MapLeftLimit - player->PlayerWindowPositionX + 1;
         game->vydt = 0;
-        if (gmae->LX >= -1) {
-            gmae->LX = 0;
+        if (game->LX >= -1) {
+            game->LX = 0;
         }
     }
 
     if (player->PlayerWindowPositionX + player->PlayerDimensionX >= map->MapRightLimit - game->vydt) {
-        gmae->LX = map->MapRightLimit - player->PlayerWindowPositionX - player->PlayerDimensionX - 1;
+        game->LX = map->MapRightLimit - player->PlayerWindowPositionX - player->PlayerDimensionX - 1;
         game->vydt = 0;
-        if (gmae->LX <= 1) {
-            gmae->LX = 0;
+        if (game->LX <= 1) {
+            game->LX = 0;
         }
     }
 
     if (player->PlayerWindowPositionY <= map->MapTopLimit - game->vydt) {
-        gmae->LX = map->MapTopLimit - player->PlayerWindowPositionY + 1;
+        game->LX = map->MapTopLimit - player->PlayerWindowPositionY + 1;
         game->vydt = 0;
-        if (gmae->LX >= -1) {
-            gmae->LX = 0;
+        if (game->LX >= -1) {
+            game->LX = 0;
         }
     }
 
     if (player->PlayerWindowPositionY + player->PlayerDimensionY >= map->MapBottomLimit - game->vydt) {
-        gmae->LX = map->MapBottomLimit - player->PlayerWindowPositionY - player->PlayerDimensionY - 1;
+        game->LX = map->MapBottomLimit - player->PlayerWindowPositionY - player->PlayerDimensionY - 1;
         game->vydt = 0;
-        if (gmae->LX) {
-            gmae->LX = 0;
+        if (game->LX) {
+            game->LX = 0;
         }
     }
 }
