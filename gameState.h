d1f681f87530172e85ed985607f08809eadff34b
felipeogesser <felipeogesser@gmail.com>
2026-04-09 15:00:52 -0300

    added left-out variables to gameState.h

--- gameState.h
+++ gameState.h
@@ -1,11 +1,15 @@
 #ifndef GAMESTATE
 #define GAMESTATE
 
+#define INVSQRT2 0.70710678f
+
 typedef struct GameState {
     float LX, LY;
     float KX, KY;
     float ZX, ZY;
     float vxdt, vydt;
+    float dirUp, dirDown, dirLeft, dirRight;
+    float vx, vy, va, vb;
 } GameState;
 
 // inline int modify_gameState(GameState *game);
