52de1ed2385374933137329515e45959c273fb29
felipeogesser <felipeogesser@gmail.com>
2026-04-10 12:20:36 -0300

    corrected wrond wording in variables

--- playerMapEdgeCollisionFunc.c
+++ playerMapEdgeCollisionFunc.c
@@ -23,18 +23,18 @@ void PlayerMapEdgeCollision(Player *player, Map *map, GameState *game) {
         }
     }
 
-    if (player->playerWindowPositionX <= map->mapTopLimit - game->vydt) {
-        game->LY = map->mapTopLimit - player->playerWindowPositionX + 1;
+    if (player->playerWindowPositionY <= map->mapTopLimit - game->vydt) {
+        game->LY = map->mapTopLimit - player->playerWindowPositionY + 1;
         game->vydt = 0;
         if (game->LY >= -1) {
             game->LY = 0;
         }
     }
 
-    if (player->playerWindowPositionX + player->playerDimensionY >= map->mapBottomLimit - game->vydt) {
-        game->LY = map->mapBottomLimit - player->playerWindowPositionX - player->playerDimensionY - 1;
+    if (player->playerWindowPositionY + player->playerDimensionY >= map->mapBottomLimit - game->vydt) {
+        game->LY = map->mapBottomLimit - player->playerWindowPositionY - player->playerDimensionY - 1;
         game->vydt = 0;
-        if (game->LY) {
+        if (game->LY <= 1) {
             game->LY = 0;
         }
     }
