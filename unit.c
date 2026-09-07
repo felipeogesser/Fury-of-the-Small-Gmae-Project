b36e7d2fe575c654ef4143e4bef67d35a19fe2ec
felipeogesser <felipeogesser@gmail.com>
2026-09-07 14:02:12 -0300

    added logic for calculating the necessary padding in unit positioning for symmetric displaying of army

--- unit.c
+++ unit.c
@@ -112,25 +112,37 @@ void set_units_position(Unit *unit, unsigned int battalion_size, Battalion *batt
     unsigned int formation_height = battalion->unit_formation_height;
     unsigned int formation_width = ceilf(battalion_size / battalion->unit_formation_height);
 
-    unsigned int padding_x = battalion->padding_between_units_x;
-    unsigned int padding_y = battalion->padding_between_units_y;
+    unsigned int unit_padding_x = battalion->padding_between_units_x;
+    unsigned int unit_padding_y = battalion->padding_between_units_y;
 
-    unsigned int cell_width = ((engine.map->mapSizeX / 2 - engine.battlefield->padding.in_between_armies / 2 - engine.battlefield->padding.left) / grid->dimension.x);
-    unsigned int cell_height = ((engine.map->mapSizeY - engine.battlefield->padding.bottom - engine.battlefield->padding.top ) / grid->dimension.y);
+    unsigned int cell_width = (engine.map->mapSizeX / 2 - engine.battlefield->padding.in_between_armies / 2 - engine.battlefield->padding.left) / grid->dimension.x;
+    unsigned int cell_height = (engine.map->mapSizeY - engine.battlefield->padding.bottom - engine.battlefield->padding.top ) / grid->dimension.y;
 
     unsigned int cell_position_x = engine.battlefield->padding.left + occupied_cell->x * cell_width;
     unsigned int cell_position_y = engine.battlefield->padding.top + occupied_cell->y * cell_height;
 
-    unsigned int cell_x = cell_position_x; //+ cell_width;
-    unsigned int cell_y = cell_position_y; //+ cell_height;
+    unsigned int cell_inner_padding_top = 0;
+    if (battalion_size < formation_height) {
+
+        cell_inner_padding_top = ((cell_height / (unit->sprite.h + unit_padding_y)) * (formation_height - battalion_size)) / 2;
+
+    } else {
+
+        cell_inner_padding_top = (cell_height - formation_height * (unit->sprite.h + unit_padding_y)) / 2;
+
+    }
+
+    unsigned int cell_inner_padding_left = 0;
+    cell_inner_padding_left = (cell_width - formation_width * (unit->sprite.w + unit_padding_x)) / 2;
+
 
     unsigned int idx = 0;
     for (unsigned int i = 0; i < formation_width; i++) {
 
         for (unsigned int j = 0; j < formation_height; j++) {
 
-            unsigned int x = cell_x + padding_x + (unit[idx].dimensionX * i);
-            unsigned int y = cell_y + padding_y + (unit[idx].dimensionY * j);
+            unsigned int x = cell_position_x + cell_inner_padding_left + unit_padding_x + (unit[idx].dimensionX * i);
+            unsigned int y = cell_position_y + cell_inner_padding_top + unit_padding_y + (unit[idx].dimensionY * j);
             unit[idx].positionX = x;
             unit[idx].positionY = y;
             idx++;
