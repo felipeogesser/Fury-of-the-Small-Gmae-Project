ac54697dc1a323182fe2fb4207c777c33a6a48da
felipeogesser <felipeogesser@gmail.com>
2026-09-17 15:36:18 -0300

    moved an exten to unit_types.h

--- unit_internal.h
+++ unit_internal.h
@@ -3,13 +3,11 @@
 
 #include "unit_types.h"
 #include "animation_internal.h"
-#include "field_entry_types.h"
 #include "sprites_internal.h"
 #include <SDL2/SDL.h>
 
 //#define MAX_unit 1024
 
-extern const FieldEntry *const unit_field_table;
 //extern const size_t unit_field_table_count;
 
 typedef struct Unit {
