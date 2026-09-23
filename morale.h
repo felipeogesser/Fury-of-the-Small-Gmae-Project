5a40fabc44c44ea35bfbaa7434b8a1837ef81855
felipeogesser <felipeogesser@gmail.com>
2026-09-23 00:26:52 -0300

    removed files that shoudant have been merged yet

--- morale.h
+++
@@ -1,8 +0,0 @@
-#ifndef MORALE_H
-#define MORALE_H
-
-#include "battalion_types.h"
-
-void morale_update(Battalion *battalion, signed int balance_of_power);
-
-#endif
--- morale_policy.h
+++
@@ -1,7 +0,0 @@
-#ifndef MORALE_POLICY_H
-#define MORALE_POLICY_H
-
-signed int evaluate_static_morale_modifiers(unsigned int modifiers);
-signed int evaluate_dynamic_morale_modifiers(unsigned int modifiers, unsigned int max_hp, unsigned int current_hp, signed int balance_of_power);
-
-#endif
